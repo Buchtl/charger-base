32ac5f9cc42cc675ddf26a3b036c6c1352bdd698
Chris <schauer@inf-schauer.de>
2025-07-31 21:03:00 +0200

    wip

--- src/charger_models_db.py
+++ src/charger_models_db.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "status_poll"
+    # in code Column(DateTime, default=datetime.utcnow)
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)  #
     time = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     eto = Column(Integer)
