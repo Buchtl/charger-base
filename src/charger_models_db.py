35f30436856d5350645fb7fe8982dcaa67b9ed4a
Chris <schauer@inf-schauer.de>
2025-07-31 21:02:08 +0200

    wip

--- src/charger_models_db.py
+++ src/charger_models_db.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, DateTime, func
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'status_poll'
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    __tablename__ = "status_poll"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)  #
+    time = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     eto = Column(Integer)
     err = Column(Integer)
-
