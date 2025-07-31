52458fdb695d637583b762d4a3d79adbe0cc8b54
Chris <schauer@inf-schauer.de>
2025-07-31 20:03:02 +0200

    refactor

--- src/charger_models.py
+++ src/charger_models.py
@@ -6,7 +6,14 @@ class error_status:
     err: int
 
 
+# energy_total, measured in Wh
 @dataclass
 class status_poll:
+    """
+    Represents the API response for polling
+    Attributes:
+        eto (int): Energy total (in Wh).
+        err (int): Error code (0 means no error).
+    """
     eto: int
     err: int
