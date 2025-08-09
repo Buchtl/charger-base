e76dd058e7bb507fff2519f1a5e00002b895ffa1
Chris <schauer@inf-schauer.de>
2025-08-09 10:34:17 +0200

    refactor test

--- tests/test_charger_api_calls.py
+++ tests/test_charger_api_calls.py
@@ -4,28 +4,29 @@ import requests_mock
 from src import charger_api_calls
 from src.charger_models import StatusPoll
 
+def mock_status(mocker):
+    freq: float = 50.01200104
+    mocker.get(charger_api_calls.api_base, json={"eto": 1, "err": 2, "tma": [3, 4], "fhz": freq})
+    expected = StatusPoll(eto=1, err=2, tma=[3, 4], fhz=freq)
+    return expected
 
 class TestChargerApiCalls(unittest.TestCase):
 
     def test_status_err(self):
         with requests_mock.Mocker() as m:
-            m.get(charger_api_calls.api_base, json={"eto": 1, "err": 2, "tma": [3, 4]})
-            expected = 2
+            expected = mock_status(m).err
             actual = charger_api_calls.status_err()
             self.assertEqual(actual, expected)
 
     def test_status_energy_total_wh(self):
         with requests_mock.Mocker() as m:
-            m.get(charger_api_calls.api_base, json={"eto": 1, "err": 2, "tma": [3, 4]})
-            expected = 1
+            expected = mock_status(m).eto
             actual = charger_api_calls.status_energy_total_wh()
             self.assertEqual(actual, expected)
 
     def test_status_polling(self):
         with requests_mock.Mocker() as m:
-            freq: float = 50.01200104
-            m.get(charger_api_calls.api_base, json={"eto": 1, "err": 2, "tma": [3, 4], "fhz":freq})
-            expected = StatusPoll(eto=1, err=2, tma=[3, 4], fhz=freq)
+            expected = mock_status(m)
             actual = charger_api_calls.status_polling()
             self.assertEqual(actual, expected)
 
